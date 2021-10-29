aab5421a777fe52103270081bafe2f373a56bf3f
Ayman Elsayeed <ayman.elsayeed@mdclone.com>
2021-10-29 13:58:48 +0300

    add unit tests.

--- Tests/TestApi.py
+++ Tests/TestApi.py
@@ -3,3 +3,18 @@ def test_get_api(get_api_client):
     """test availability of the api"""
     client = get_api_client
     assert client.get("/").status_code == 200
+
+
+def test_bar_plot(get_api_client):
+    response = get_api_client.get("/view_bar_plot")
+    assert response.status_code == 200
+
+
+def test_line_plot(get_api_client):
+    response = get_api_client.get("/view_line_plot")
+    assert response.status_code == 200
+
+
+def test_view_scatter_plot(get_api_client):
+    response = get_api_client.get("/view_scatter_plot")
+    assert response.status_code == 200
