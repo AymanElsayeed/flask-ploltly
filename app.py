29df168ea2e3b4b79b2ed68a45a7c1a992554436
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-07-30 14:53:41 +0300

    temp commit.

--- app.py
+++ app.py
@@ -3,11 +3,46 @@ import os
 from flask import Flask, url_for, jsonify, render_template, request
 import json
 import plotly
+import asyncio
 import argparse
 from src.Plots import *
 from src.config import FactoryConfigClass
 
 app = Flask(__name__)
+
+
+# @app.before_request
+async def request_params(params: dict) -> dict:
+    data = {}
+    countries = unique_countries()
+    numeric_columns = float_columns()
+    cat_columns = categorical_columns()
+
+    x = params.get('X', numeric_columns[0])
+    y = params.get('Y', numeric_columns[1])
+    size = params.get('size', numeric_columns[2])
+
+    color = params.get('color', cat_columns[0])
+
+    try:
+        column, sub_group = request.form.get('option', '').split(',')
+    except ValueError:
+        column = request.form.get('option', '').split(',')
+
+    mode = request.form.get('mode', "lines+markers")
+    country = request.form.get('group', countries[0])
+
+    if mode:
+        data['mode'] = mode
+
+    if column:
+        data['column'] = column
+        # data['sub_group'] = sub_group
+
+    data.update(dict(x=x, y=y, color=color, size=size, country=country, numeric_columns=numeric_columns,
+                     cat_columns=cat_columns, countries=countries))
+
+    return data
 
 
 @app.route('/home')
@@ -20,9 +55,13 @@ def home():
 
 
 @app.route('/view_bar_plot', methods=["GET", "POST"])
-def view_bar_plot():
-    countries = unique_countries()
-    numeric_columns = float_columns()
+async def view_bar_plot():
+    data = await request_params(request.form)
+    countries = data.get('countries')
+    numeric_columns = data.get('numeric_columns')
+    country = data.get('group', countries[0])
+    # x = request.form.get('X', numeric_columns[0])
+    # y = request.form.get('Y', numeric_columns[1])
 
     country = request.form.get('group', countries[0])
     x = request.form.get('X', numeric_columns[0])
@@ -30,26 +69,38 @@ def view_bar_plot():
 
     fig = bar_plot(x=x, y=y, country=country)
     graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    fig.write_image("static/fig2.svg")
+    paths = ['static/fig2.svg', 'static/line.svg', 'static/scatter.svg'] * 10
     return render_template('bar_plot.html', graph_json=graph_json, Groups=countries, Y=numeric_columns,
-                           X=numeric_columns, title="Bar Plot page")
+                           X=numeric_columns, title="Bar Plot page", spath='static/fig2.svg', paths=paths)
+
+
+@app.route('/view_report', methods=["GET", "POST"])
+async def view_report():
+    figure_classes = [f"g{i}{k}" for i in range(1, 5) for k in range(1, 4)]
+    paths = ['static/fig2.svg', 'static/line.svg', 'static/scatter.svg'] * 4
+    fig_data = zip(figure_classes, paths)
+    return render_template('report.html', title="Bar Plot page", spath='static/fig2.svg', data=fig_data)
 
 
 @app.route('/view_scatter_plot', methods=["GET", "POST"])
-def view_scatter_plot():
+async def view_scatter_plot():
+    data = await request_params(request.form)
     numeric_columns = float_columns()
     cat_columns = categorical_columns()
 
-    x = request.form.get('X', numeric_columns[0])
-    y = request.form.get('Y', numeric_columns[1])
-    size = request.form.get('size', numeric_columns[2])
-    color = request.form.get('color', cat_columns[0])
+    x = data.get('X', numeric_columns[0])
+    y = data.get('Y', numeric_columns[1])
+    size = data.get('size', numeric_columns[2])
+    color = data.get('color', cat_columns[0])
 
     fig = scatter_plot(x=x, y=y, color=color, size=size)
+    fig.write_image("static/scatter.svg", height=300, width=300)
 
     scatter_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('scatter_plot.html', graph_json=scatter_json, X=numeric_columns, Y=numeric_columns,
-                           Colors=cat_columns, Sizes=numeric_columns, title="Scatter Plot Graph")
+                           Colors=cat_columns, Sizes=numeric_columns, title="Scatter Plot Graph", spath='fig1.svg')
 
 
 @app.route('/view_line_plot', methods=["GET", "POST"])
@@ -67,7 +118,7 @@ def view_line_plot():
     options = unique_subgroup()
 
     fig = line_plot(x=x, y=y, color=color, mode=mode, column=column, sub_group=sub_group)
-
+    fig.write_image("static/line.svg", height=300, width=300)
     line_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('line_plot.html', graph_json=line_json,
@@ -106,4 +157,4 @@ if __name__ == '__main__':
                         required=False, default='dev')
     args = parser.parse_args()
     app.config.from_object(FactoryConfigClass(env=args.environment))
-    app.run('0.0.0.0', 5000, debug=True, threaded=True, use_reloader=True)
+    app.run('0.0.0.0', 5001, debug=True, threaded=True, use_reloader=True)
--- src/Plots.py
+++ src/Plots.py
@@ -2,6 +2,12 @@
 Basic Plots base on gap-minder data set.
 """
 import plotly.express as px
+import asyncio
+import plotly.io as pio
+png_renderer = pio.renderers["svg"]
+png_renderer.width = 500
+png_renderer.height = 500
+pio.renderers.default = "svg"
 
 
 def unique_countries():
@@ -12,6 +18,13 @@ def unique_countries():
     df = px.data.gapminder()
     countries = df.country.unique().tolist()
     return countries
+
+
+def unique_subgroup():
+    df = px.data.gapminder()
+    s = df.select_dtypes(exclude=[float, int])
+    sub_group = s.apply(lambda x: x.unique().tolist()).to_dict()
+    return sub_group
 
 
 def unique_continent():
@@ -63,6 +76,15 @@ def float_columns():
     return df.select_dtypes(include=[float, int]).columns.to_list()
 
 
+def categorical_columns():
+    """
+    float columns in the data set.
+    :return: list
+    """
+    df = px.data.gapminder()
+    return df.select_dtypes(exclude=[float, int]).columns.to_list()
+
+
 def bar_plot(x: str = 'year', y: str = 'pop', country: str = 'New Zealand'):
     """
     Generate Bar Plot
@@ -72,21 +94,23 @@ def bar_plot(x: str = 'year', y: str = 'pop', country: str = 'New Zealand'):
     :return: figure
     """
     nz = px.data.gapminder().query(f"country == '{country}'")
-    fig = px.bar(nz, x=x, y=y, title=f"{country} Population over time")
+    title = f"{country} Population over time"
+    fig = px.bar(nz, x=x, y=y, title="", height=300, width=300)
     return fig
 
 
-def line_plot(x='year', y='lifeExp', color='country', continent='Oceania', mode="lines+markers"):
+def line_plot(x: str, y: str, color: str, sub_group: str, mode: str, column: str):
     """
     Generate Line Plot
     :param x:
     :param y:
     :param color:
-    :param continent:
+    :param sub_group:
+    :param column:
     :param mode:
     :return: figure
     """
-    df = px.data.gapminder().query(f"continent=='{continent}'")
+    df = px.data.gapminder().query(f"{column}=='{sub_group}'")
     fig = px.line(df, x=x, y=y, color=color)
     fig.update_traces(mode=mode)
     return fig
@@ -102,7 +126,7 @@ def scatter_plot(x='lifeExp', y='gdpPercap', color='country', size='pop'):
     :return: figure
     """
     df = px.data.gapminder()
-    fig = px.scatter(df, x=x, y=y, color=color, size=size)
+    fig = px.scatter(df, x=x, y=y, color=color, size=size, render_mode='svg')
     return fig
 
 
